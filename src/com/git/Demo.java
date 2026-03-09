c6a52bde639630ec3ede1897a3159dc4558c6711
AnoushkaBhadra <anoushkabhadra@gmail.com>
2026-03-09 12:10:48 +0530

    Updated initial file

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -4,8 +4,9 @@ public class Demo {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 14;
+		int c = 20;
 		
-		int sum = a+b;
+		int sum = a+b+c;
 		
 		System.out.println(sum);
 	}
